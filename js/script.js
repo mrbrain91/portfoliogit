f832f7ecec5e57a21c6de2a7b5be6cea6692b039
mrbrain91 <39298381+mrbrain91@users.noreply.github.com>
2022-04-04 21:21:20 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+const counters = document.querySelectorAll('.experience__degree-counter'),
+    lines = document.querySelectorAll('.experience__degree-percent-shadow');
+
+console.log(counters);
